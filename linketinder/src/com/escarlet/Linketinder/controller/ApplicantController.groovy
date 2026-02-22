f0d5a200b051777e9c5088325726efe9ecca2368
Escarlet Lima <escarletlimaa@gmail.com>
2026-02-22 18:46:30 -0400

    feat(controller): implement applicant creation and validate list insertion

--- linketinder/src/com/escarlet/Linketinder/controller/ApplicantController.groovy
+++ linketinder/src/com/escarlet/Linketinder/controller/ApplicantController.groovy
@@ -13,4 +13,22 @@ class ApplicantController {
     List<Applicant> list() {
         return this.applicantsList
     }
+
+    void register (Applicant newApplicant) {
+        if (newApplicant == null) {
+            throw new IllegalArgumentException("O candidato não pode ser nulo")
+        }
+
+        boolean invalidAge = newApplicant.age <= 0
+        boolean invalidCompetences = newApplicant.competences == null || newApplicant.competences.isEmpty()
+        boolean invalidStrings = newApplicant.name == (null) || newApplicant.name.isBlank() || newApplicant.email == null || newApplicant.email.isBlank()
+
+        if (invalidStrings || invalidCompetences)
+            throw new IllegalArgumentException("Nome, email e competências são campos obrigatórios.")
+
+        if (invalidAge)
+            throw  new IllegalArgumentException("A idade não pode ser menor ou igual a 0")
+
+        this.applicantsList.add(newApplicant)
+    }
 }
--- linketinder/src/test/com/escarlet/Linketinder/controller/ApplicantControllerTest.groovy
+++ linketinder/src/test/com/escarlet/Linketinder/controller/ApplicantControllerTest.groovy
@@ -1,16 +1,41 @@
 package com.escarlet.Linketinder.controller
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.BeforeEach
 import static org.junit.jupiter.api.Assertions.*
+import com.escarlet.Linketinder.model.entities.Applicant
 
 class ApplicantControllerTest {
+    private ApplicantController controller
+
+    @BeforeEach
+    void setUp() { this.controller = new ApplicantController() }
+
     @Test
     void "Deve retornar a listagem com pelo menos 5 candidatos pré-cadastrados " () {
-        def controller = new ApplicantController()
         def list = controller.list()
 
         assertNotNull(list, "A lista de candidatos não pode ser nula")
         assertTrue(list.size() >=5, "A lista deve conter pelo menos 5 candidatos pré-cadastrados")
     }
 
+    @Test
+    void "Deve adicionar um novo candidato a lista com sucesso"() {
+        int initSize = controller.list().size()
+
+        def applicantMock = new Applicant(
+                name: "Candidato1",
+                email: "candidato1@email.com",
+                cpf: "032.123.332-43",
+                age: 26,
+                state: "SP",
+                cep: "8300-127",
+                description: "Estudante de Engenharia de software focada em backend.",
+                competences: ["Groovy", "Git", "Linux"]
+        )
+        controller.register(applicantMock)
+
+        assertEquals(initSize + 1, controller.list().size())
+        assertTrue(controller.list().contains(applicantMock))
+    }
 }
